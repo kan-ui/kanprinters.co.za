764fbda39a270967403913eff68d193987bbae03
Adv-Fieks <shafiek@advontier.com>
2026-01-26 11:08:21 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemas/index'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
 export default defineConfig({
   name: 'kan-printers',
   title: 'Kan Printers CMS',
   
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId,
+  dataset,
   
   basePath: '/studio',
   
