37c127f62a016e8977bd15a53cb3009405965784
Adv-Fieks <shafiek@advontier.com>
2026-01-26 13:56:59 +0200

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,57 +1,71 @@
 import Image from "next/image";
 import React from "react";
 import { Container } from "@/components/Container";
+import { urlFor } from "@/lib/sanity.image";
 
 import userOneImg from "../../public/img/user1.jpg";
 import userTwoImg from "../../public/img/user2.jpg";
 import userThreeImg from "../../public/img/user3.jpg";
 
-export const Testimonials = () => {
+interface TestimonialsProps {
+  testimonialsData?: Array<{
+    quote: string;
+    author: string;
+    role?: string;
+    company?: string;
+    image?: any;
+  }>;
+}
+
+export const Testimonials = ({ testimonialsData }: TestimonialsProps) => {
+  // Default testimonials if no Sanity data
+  const defaultTestimonials = [
+    {
+      quote: "Share a real testimonial that hits some of your benefits from one of your popular customer.",
+      author: "Sarah Steiner",
+      role: "VP Sales at Google",
+      image: userOneImg,
+    },
+    {
+      quote: "Make sure you only pick the right sentence to keep it short and simple.",
+      author: "Dylan Ambrose",
+      role: "Lead marketer at Netflix",
+      image: userTwoImg,
+    },
+    {
+      quote: "This is an awesome landing page template I've seen. I would use this for anything.",
+      author: "Gabrielle Winn",
+      role: "Co-founder of Acme Inc",
+      image: userThreeImg,
+    },
+  ];
+
+  const testimonials = testimonialsData && testimonialsData.length > 0 
+    ? testimonialsData.map((t: any) => ({
+        quote: t.quote,
+        author: t.author,
+        role: t.role || t.company || "",
+        image: t.image?.asset ? urlFor(t.image).width(40).height(40).url() : userOneImg,
+      }))
+    : defaultTestimonials;
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="lg:col-span-2 xl:col-auto">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Share a real <Mark>testimonial</Mark>
-              that hits some of your benefits from one of your popular customer.
-            </p>
+        {testimonials.map((testimonial: any, index: number) => (
+          <div key={index} className={index === 0 ? "lg:col-span-2 xl:col-auto" : ""}>
+            <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+              <p className="text-2xl leading-normal">
+                {testimonial.quote}
+              </p>
 
-            <Avatar
-              image={userOneImg}
-              name="Sarah Steiner"
-              title="VP Sales at Google"
-            />
+              <Avatar
+                image={testimonial.image}
+                name={testimonial.author}
+                title={testimonial.role}
+              />
+            </div>
           </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Make sure you only pick the <Mark>right sentence</Mark>
-              to keep it short and simple.
-            </p>
-
-            <Avatar
-              image={userTwoImg}
-              name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
-            />
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              This is an <Mark>awesome</Mark> landing page template I&apos;ve
-              seen. I would use this for anything.
-            </p>
-
-            <Avatar
-              image={userThreeImg}
-              name="Gabrielle Winn"
-              title="Co-founder of Acme Inc"
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </Container>
   );
