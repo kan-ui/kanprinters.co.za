0b5c88f86570097cb217ed4fffe8d263b4832d78
Adv-Fieks <shafiek@advontier.com>
2026-01-26 10:54:06 +0200

    Create index.ts

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,29 @@
+import service from './service'
+import portfolio from './portfolio'
+import testimonial from './testimonial'
+import faq from './faq'
+import teamMember from './teamMember'
+import value from './value'
+import siteSettings from './siteSettings'
+import contactInfo from './contactInfo'
+import socialMedia from './socialMedia'
+import heroSection from './heroSection'
+import companyHistory from './companyHistory'
+import ctaSection from './ctaSection'
+
+export const schemaTypes = [
+  // Site-wide settings
+  siteSettings,
+  contactInfo,
+  socialMedia,
+  heroSection,
+  companyHistory,
+  ctaSection,
+  // Content types
+  service,
+  portfolio,
+  testimonial,
+  faq,
+  teamMember,
+  value,
+]
